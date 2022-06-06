abe2a3e9a2e3a4a3f257ed970cf1b1361c8ae855
“ggmcjy94” <lhs01071526524@gmail.com>
2022-06-06 21:19:53 +0900

    빌더패턴

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,11 +1,14 @@
 public class Client {
 
     public static void main(String[] args) {
-        Member member = new Builder("hyunseok")
+        Member member = new Member.Builder()
+                .name("hyunseok")
                 .age(25)
                 .country("korea")
-                .job("Student")
                 .build();
+
+        System.out.println(member);
+
 
     }
 }
--- src/main/java/Member.java
+++ src/main/java/Member.java
@@ -1,13 +1,68 @@
 public class Member {
+
     private String name;
     private int age;
     private String job;
     private String gender;
     private String country;
 
-    public Member(Builder builder) {
+
+    Member(Builder builder) {
+        this.name = builder.name;
+        this.age = builder.age;
+        this.job = builder.job;
+        this.gender = builder.gender;
+        this.country = builder.country;
     }
 
 
+    public static class Builder {
 
+        private String name;
+        private int age;
+        private String job;
+        private String gender;
+        private String country;
+
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder age(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public Builder job(String job) {
+            this.job = job;
+            return this;
+        }
+
+        public Builder gender(String gender) {
+            this.gender = gender;
+            return this;
+        }
+
+        public Builder country(String country) {
+            this.country = country;
+            return this;
+        }
+
+        public Member build() {
+            return new Member(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", job='" + job + '\'' +
+                ", gender='" + gender + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
 }
